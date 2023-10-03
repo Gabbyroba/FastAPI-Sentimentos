3a28ef1519bc8d57c315a54aabfde04620f322b9
Gabriella Ramos <115726002+Gabbyroba@users.noreply.github.com>
2023-10-03 12:42:33 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+# Defina as origens permitidas
+origins = [
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5500/",
+    "http://127.0.0.1:5500/index.html",
+    "https://feelingyou.netlify.app",
+]
+
+# Configurar o middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client_id = '88280c7374b446b39db78a9bd3bf3ca8'
+client_secret = '72ee752dee1146e4b1c6ac80cc1947c2'
+
+# Autenticação para obter um token de acesso
+token_url = 'https://accounts.spotify.com/api/token'
+
+data = {
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret,
+}
+
+# Faça a solicitação POST para obter o token de acesso
+response = requests.post(token_url, data=data)
+
+# Verifique se a solicitação foi bem-sucedida
+if response.status_code == 200:
+    # O token de acesso está no campo 'access_token' na resposta JSON
+    token_data = response.json()
+    access_token = token_data['access_token']
+    print(f'Token de acesso: {access_token}')
+else:
+    print('Falha ao obter o token de acesso')
+
+@app.get("/")
+async def definir_home():
+    return('Boas vindas à API feeling you!')    
+
+@app.post("/analisar-sentimento")
+async def analisar_sentimento(data: dict):
+    # Receba o sentimento do usuário
+    sentimento = data.get("sentimento", "").lower()
+
+    # Defina o endpoint da API do Spotify para buscar músicas
+    search_url = 'https://api.spotify.com/v1/search'
+    
+    # Especifique os parâmetros da pesquisa, como o sentimento ou o gênero da música
+    params = {
+        'q': sentimento,
+        'type': 'track',
+        'limit': 1  # Limite de resultados
+    }
+
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    # Faça a solicitação à API do Spotify
+    response = requests.get(search_url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Extraia os detalhes da música a partir da resposta
+        # Por exemplo, data['tracks']['items'][0]['name'] contém o nome da música
+        musica_sugerida = data['tracks']['items'][0]['name']
+        return {"sentimento": sentimento, "musica_sugerida": musica_sugerida}
+    else:
+        return {"erro": "Não foi possível encontrar uma música para o sentimento especificado"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
